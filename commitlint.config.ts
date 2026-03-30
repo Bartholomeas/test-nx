3f12d9ac158e72a21459d090007052194cc3f9f6
Bartosz Stefaniak <bstefaniak@estrategysolutions.com>
2026-03-30 21:07:40 +0200

    fix(all): default changelog option

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,15 +1,10 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-   rules: {
+  rules: {
     'scope-enum': [
       2,
       'always',
-      [
-        'users2-backend',
-        'mng-admin-backend',
-        'common',
-        'release',
-      ],
+      ['users2-backend', 'mng-admin-backend', 'common', 'release', 'all'],
     ],
     'scope-empty': [1, 'never'],
   },
