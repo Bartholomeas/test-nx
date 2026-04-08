38ccdf3693adc8e340ffdc40c0e783916ec5559b
Bartosz Stefaniak <bstefaniak@estrategysolutions.com>
2026-04-08 12:45:14 +0200

    feat(users2-backend, common): test releas

--- apps/users2-backend/src/main.ts
+++ apps/users2-backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app/app.module';
 
 // Comment test c
 // Comment test c
-// Promote to release prod ok
+// Promote to release prod ok fffff
 // Promote to release prod ok test test
 
 bootstrapApp({
